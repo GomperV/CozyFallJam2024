e4dff0853bd8de0635704c773f5ffd8e8ae0d021
Jakub Konior <karfaj45@gmail.com>
2024-10-25 12:47:35 +0200

    added Defender & pause

---
+++ Assets/EnemyDefenderMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDefenderMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject PunktB, PunktA;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+    public float patrolSpeed;
+    private bool facingRight = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        patrolSpeed = Random.Range(1f, 2f);
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = PunktB.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentPoint == PunktB.transform)
+        {
+            rb.velocity = new Vector2(patrolSpeed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-patrolSpeed, 0);
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PunktB.transform)
+        {
+            currentPoint = PunktA.transform;
+            if (facingRight)
+            {
+                transform.Rotate(0f, 180f, 0f);
+                facingRight = false;
+            }
+
+
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PunktA.transform)
+        {
+            currentPoint = PunktB.transform;
+            if (!facingRight)
+            {
+                transform.Rotate(0f, 180f, 0f);
+                facingRight = true;
+            }
+        }
+    }
+}
--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 using TMPro;
 public class UIManager : MonoBehaviour
 {
+    private bool changePauseMode = true;
     [SerializeField]
     private GameObject gameInfoPanel;
     [SerializeField]
@@ -27,9 +28,29 @@ public class UIManager : MonoBehaviour
         infoText.text = "YOU'VE LOST!";
         Time.timeScale = 0f;
     }
-    // Update is called once per frame
-    void Update()
+
+    public void activate_pausemenu(bool isPaused)
     {
-        
+        if (isPaused)
+        {
+            infoText.text = "GAME PAUSED";
+            gameInfoPanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            infoText.text = "";
+            gameInfoPanel.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            activate_pausemenu(changePauseMode);
+            changePauseMode = !changePauseMode;
+        }
     }
 }
