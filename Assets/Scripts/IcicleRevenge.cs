a16839f1a888c4b09b8ad4d038115cccb837c7f3
Herman Groenenboom <taartaar@gmail.com>
2024-10-27 12:27:42 +0100

    Added icicle revenge projectile

---
+++ Assets/Scripts/IcicleRevenge.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+
+using UnityEngine;
+
+namespace Basics
+{
+    public class IcicleRevenge : MonoBehaviour
+    {
+        public float speed = 10f;
+        public float fireDelay = 1f;
+        public float jitter = 0.1f;
+        public Transform particleLocation;
+        public GameObject particlePrefab;
+
+        private Rigidbody2D _rb;
+        private Vector2 startPos;
+
+        IEnumerator Start()
+        {
+            startPos = transform.position;
+            _rb = GetComponent<Rigidbody2D>();
+            var player = FindObjectOfType<PlayerController>();
+
+            Vector2 direction = Vector2.right;
+            float start = Time.time;
+            while(Time.time < start + fireDelay)
+            {
+                transform.position = startPos + Random.insideUnitCircle*jitter;
+                direction = player.transform.position - transform.position;
+                float angle = Vector2.SignedAngle(Vector2.down, direction);
+                transform.rotation = Quaternion.Euler(0f, 0f, angle);
+                yield return null;
+            }
+
+            _rb.velocity = direction.normalized*speed;
+        }
+
+        public void DestroyBullet()
+        {
+            var particle = Instantiate(particlePrefab, null);
+            particle.transform.position = particleLocation.position;
+            particle.transform.rotation = transform.rotation;
+
+            Destroy(gameObject);
+        }
+    }
+}
